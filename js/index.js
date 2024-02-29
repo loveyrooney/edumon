37a27327032a4ed3df551af602a81f32eb3741f0
rooney <loveyrooney@gmail.com>
2024-02-29 13:56:52 +0900

    [kim/fix] nav바 반응형 js파일 코드수정

--- js/index.js
+++ js/index.js
@@ -1,41 +1,77 @@
+const book_hov = document.querySelector('.book');
 const content_hov = document.querySelector('.contents_hov');
 const service_hov = document.querySelector('.service_hov');
 const contents = document.querySelector('#contents');
-const service = document.querySelector('#service');
-const hamburger = document.querySelector('.hamburger');
-const nav1 = document.querySelector('#nav1');
+let service = document.querySelector('#service');
+let hamburger = document.querySelector('#hamburger');
+let nav1 = document.querySelector('#nav1');
+let m_li = document.querySelector('#m_li');
 
-if(window.innerWidth < 768) {
-    service.className = 'm_service_box';
-    nav1.className = 'd-none';
-} else {
-    service.className = 'd-none';
+let timer = null;
+
+function clickHamburger(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (service.className === 'hide') {
+        nav1.className = 'm_nav1';
+        service.className = 'm_service_box';
+        m_li.className = 'm_li';
+    } else {
+        nav1.className = 'hide';
+        service.className = 'hide';
+    }
 }
 
+function bookMouseenter() {
+    if(window.innerWidth>768){
+        service.className = 'hide';
+        contents.className = 'hide';
+    }
+}
+
+function contentsMouseenter() {
+    if(window.innerWidth>768){
+        service.className = 'hide';
+        contents.className = 'contents_box';
+    }
+}
+
+function serviceMouseenter() {
+    if(window.innerWidth >768) {
+        contents.className = 'hide';
+        service.className = 'service_box';
+    }
+}
+
+function contentsMouseleave() {
+    if(window.innerWidth>768) contents.className = "hide";
+}
+
+function serviceMouseleave() {
+    if(window.innerWidth>768) service.className = 'hide';
+}
+
+function classSet() {
+    if (window.innerWidth < 768) {
+        nav1.className = 'hide';
+        service.className = 'hide';
+        hamburger.addEventListener('click', clickHamburger);
+    } else {
+        nav1.className = 'nav1';
+        service.className = 'hide';
+    }
+    book_hov.addEventListener('mouseenter', bookMouseenter);
+    content_hov.addEventListener('mouseenter', contentsMouseenter);
+    service_hov.addEventListener('mouseenter', serviceMouseenter);
+    contents.addEventListener('mouseleave', contentsMouseleave);
+    service.addEventListener('mouseleave', serviceMouseleave);
+}
+
+//브라우저 사이즈 변경시
 window.addEventListener('resize', function () {
-        if(window.innerWidth < 768) service.className = 'm_service_box';
-        else service.className = 'd-none';
-
-        content_hov.addEventListener('mouseenter', function () {
-            if(window.innerWidth >=768) {
-                service.className = 'd-none';
-                contents.className = 'contents_box';
-            }
-        });
-
-        service_hov.addEventListener('mouseenter', function () {
-            if(window.innerWidth >= 768) {
-                contents.className = 'd-none';
-                service.className = 'service_box';
-            }
-        });
-
-        contents.addEventListener('mouseleave', function () {
-            contents.className = "d-none";
-        });
-
-        service.addEventListener('mouseleave', function () {
-            if (window.innerWidth >= 768) service.className = "d-none";
-        });
+    clearTimeout(timer);
+    timer = setTimeout(classSet, 200);
 });
 
+//첫 시작 화면
+classSet();
