a8e53c2ab5d6126d489cc0f8d32ac474ca5c3ab9
star <ebyeol628@gmail.com>
2024-03-02 00:16:18 +0900

    [eun/feat] cart.html 이미지 추가, cart.css 수정 및 추가, cart.js 기능 추가

--- js/cart.js
+++ js/cart.js
@@ -15,40 +15,28 @@ for(let i=0; i < acc.length; i++) {
     });
 }
 
-let tot_price;
-
-
-
-
-    // let price=Number(document.getElementById("price").value.replace());
-    //
-    // tot_price=price*Number(n.value);
-    // document.getElementById("tot_price").value=tot_price;
-
+/* 가격 계산 */
  let check_choice=document.querySelectorAll('.choice');
-
 
  let fn=function(event) {
      let ch_check_div = event.target.parentElement.parentElement;
      let child_check_div= ch_check_div.children;
 
-      let price=child_check_div[2].querySelectorAll('.price');
-      let su=child_check_div[3].querySelectorAll('.num');
+      let price=child_check_div[3].querySelectorAll('.price');
+      let su=child_check_div[4].querySelectorAll('.num');
 
       //console.log(price[0].value);
       //console.log(su[0].value);
 
-
-      let pr=Number(price[0].value.replace(',',''))
-      let tot
-
+      let book_price=Number(price[0].value.replace(',',''));
+      let tot;
 
        su[0].onchange=function(e){
-             //console.log(e.target.value);
-           tot=pr * Number(e.target.value)
+
+           tot=Number(book_price) * Number(e.target.value);
 
            document.getElementById("tot_price").value
-           = tot
+           = tot.toLocaleString();
        }
 
  }
@@ -57,15 +45,30 @@ let tot_price;
  });
 
 
+ /* 쇼핑 계속하기 */
+document.getElementById("shopping").onclick=function (){
+    location.replace('product.html')
+}
 
+ /* 선택 삭제 */
+document.getElementById("del").addEventListener('click',function (){
+    let check1=document.getElementById("check").checked;
+    let check2=document.getElementById("check2").checked;
+   
+    if(check1===false&&check2===false){
+        alert('하나 이상의 상품을 선택해 주세요.');
+    }else{
+        confirm("선택하신 상품을 삭제하시겠습니까?");
+    }
+});
 
+/* 주문하기 */
+document.getElementById("order").addEventListener('click',function (){
+    let check1=document.getElementById("check").checked;
+    let check2=document.getElementById("check2").checked;
 
-
-
-
-
-
-
-
-
+    if(check1===false&&check2===false) {
+        alert('하나 이상의 상품을 선택해 주세요.');
+    }
+});
 
