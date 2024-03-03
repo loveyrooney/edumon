2a05336eb1151c347d435605da7360ab65c2eae7
zlzonminnie <kyungmin1025@kakao.com>
2024-03-03 16:04:15 +0900

    [min/fix] superior css,html,js 수정 및 재작성

--- js/superior.js
+++ js/superior.js
@@ -1,40 +1,20 @@
-const mytabs=document.querySelectorAll('.tabItem');
-const tabpanels=document.querySelectorAll('.tabpanel');
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-const init=function(){
-    let initli=mytabs[0].firstElementChild;
-    let initA=initli.parentElement.getAttribute('aria-controls');
-    initli.classList.add('bgpanel');
-    initli.focus();
-    let initId=document.getElementById(initA);
-    initId.classList.add('visit');
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
-
-init();
-
-mytabs.forEach(item=>{
-    item.firstElementChild.addEventListener('focus', function(e){
-        e.preventDefault();
-
-        for(let i=0; i<mytabs.length; i++)
-        {
-            mytabs[i].firstElementChild.classList.remove('bgpanel');
-        }
-
-        for(let i=0; i<tabpanels.length; i++)
-        {
-            tabpanels[i].classList.remove('visit');
-        }
-        e.target.classList.add('bgpanel');
-        let data=e.target.parentElement.getAttribute('aria-controls');
-
-        console.log(data);
-
-        if(data==null)
-            data='tabpanel-1';
-        document.getElementById(data).classList.add('visit');
-
-    });
-
-
-})
