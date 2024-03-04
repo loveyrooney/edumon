2624edb2e8fb8f42e479d9aa6444743f1fc080f3
rooney <loveyrooney@gmail.com>
2024-03-04 14:04:21 +0900

    [kim/feat] nav바 tab 기능 추가

--- js/index.js
+++ js/index.js
@@ -7,39 +7,59 @@ let hamburger = document.querySelector("#hamburger");
 let nav1 = document.querySelector("#nav1");
 let m_li1 = document.querySelector("#m_li1");
 let m_li2 = document.querySelector("#m_li2");
+let sub = document.querySelector('#sub');
+let info = document.querySelector('#info');
 
 let timer = null;
 
 const clickHamburger = function (e) {
   e.preventDefault();
   e.stopPropagation();
-  if (service.className === "hide") {
-    nav1.className = "m_nav1";
-    m_li1.className = "m_li";
-    m_li2.className = "m_li";
+  if(e.type=='click' || e.keyCode == 9){
+    if (nav1.className === "hide") {
+      nav1.className = "m_nav1";
+      m_li1.className = "m_li";
+      m_li2.className = "m_li";
+    } else {
+      nav1.className = "hide";
+    }
+  }
+};
+
+const bookMouseenter = function (e) {
+  if (window.innerWidth > 768) {
+    service.className = "hide";
+    contents.className = "hide";
   } else {
-    nav1.className = "hide";
+    if(window.innerWidth <768){
+      e.target.addEventListener('keyup',function (e){
+        if(e.keyCode == 38) hamburger.focus();
+      });
+    }
   }
 };
 
-const bookMouseenter = function () {
-  if (window.innerWidth > 768) {
-    service.className = "hide";
-    contents.className = "hide";
+const contentsMouseenter = function (e) {
+  if (e.type=='mouseenter' || e.keyCode == 9) {
+    if(window.innerWidth > 768){
+      service.className = "hide";
+      contents.className = "contents_box";
+    }
+    e.target.addEventListener('keyup',function (e){
+      if(e.keyCode == 40) sub.focus();
+    });
   }
 };
 
-const contentsMouseenter = function () {
-  if (window.innerWidth > 768) {
-    service.className = "hide";
-    contents.className = "contents_box";
-  }
-};
-
-const serviceMouseenter = function () {
-  if (window.innerWidth > 768) {
-    contents.className = "hide";
-    service.className = "service_box";
+const serviceMouseenter = function (e) {
+  if (e.type=='mouseenter' || e.keyCode == 9) {
+    if(window.innerWidth > 768){
+      contents.className = "hide";
+      service.className = "service_box";
+    }
+    e.target.addEventListener('keyup',function (e){
+      if(e.keyCode == 40) info.focus();
+    });
   }
 };
 
@@ -51,16 +71,30 @@ const serviceMouseleave = function () {
   if (window.innerWidth > 768) service.className = "hide";
 };
 
+const subKeyup = function (e) {
+  if(e.keyCode == 38) content_hov.firstElementChild.focus();
+}
+
+const infoKeyup = function (e) {
+  if(e.keyCode == 38) service_hov.firstElementChild.focus();
+}
+
 const classSet = function () {
   if (window.innerWidth < 768) {
     nav1.className = "hide";
     hamburger.addEventListener("click", clickHamburger);
+    hamburger.addEventListener('keyup',clickHamburger);
   } else {
     nav1.className = "nav1";
   }
   book_hov.addEventListener("mouseenter", bookMouseenter);
+  book_hov.addEventListener("keyup",bookMouseenter);
   content_hov.addEventListener("mouseenter", contentsMouseenter);
+  content_hov.addEventListener("keyup", contentsMouseenter);
+  sub.addEventListener('keyup',subKeyup);
   service_hov.addEventListener("mouseenter", serviceMouseenter);
+  service_hov.addEventListener("keyup", serviceMouseenter);
+  info.addEventListener('keyup',infoKeyup);
   contents.addEventListener("mouseleave", contentsMouseleave);
   service.addEventListener("mouseleave", serviceMouseleave);
 };
